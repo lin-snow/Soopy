8d973500521d82916fffae329d6b33edc58f8874
L1nSn0w <l1nsn0w@qq.com>
2024-11-15 20:00:37 +0800

    添加多个 SVG 图标文件，更新头像大小和样式，增加 fun 按钮功能及相关样式

---
+++ Assets/scripts/base.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('base.js loaded');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const funbutton = document.getElementById('fun');
+    funbutton.addEventListener('click', function() {
+        const links = document.getElementById('links');
+        const linksTitle = document.getElementById('linksTitle');
+        const projects = document.getElementById('projects');
+        const projectsTitle = document.getElementById('projectsTitle');
+        const resume = document.getElementById('resume');
+
+        links.classList.toggle('hidden');
+        linksTitle.classList.toggle('hidden');
+        projects.classList.toggle('hidden');
+        projectsTitle.classList.toggle('hidden');
+        resume.classList.toggle('hidden');
+    })
+});
